2b735a7412fda3a28ee7d75749c92f78feee4747
sdcwdcee <srdjan.dzombic@wirecard.com>
2018-01-23 09:36:18 +0100

    #39 Delete ratepayscript from wcp.php

--- includes/modules/payment/wcp.php
+++ includes/modules/payment/wcp.php
@@ -116,14 +116,7 @@ class wcp_core{
 		    $_SESSION['wcp-consumerDeviceId'] = $consumerDeviceId;
 	    }
 
-        if($this->payment_type == 'INVOICE' && wcp_core::constant("MODULE_PAYMENT_WCP_INVOICE_PROVIDER") == "RatePay" ||
-            $this->payment_type == 'INSTALLMENT' && wcp_core::constant("MODULE_PAYMENT_WCP_INSTALLMENT_PROVIDER") == "RatePay")
-        {
-            $ratepay = '<script language="JavaScript">var di = {t:"'.$consumerDeviceId.'",v:"WDWL",l:"Checkout"};</script>';
-            $ratepay .= '<script type="text/javascript" src="//d.ratepay.com/'.$consumerDeviceId.'/di.js"></script>';
-            $ratepay .= '<noscript><link rel="stylesheet" type="text/css" href="//d.ratepay.com/di.css?t='.$consumerDeviceId.'&v=WDWL&l=Checkout"></noscript>';
-            $ratepay .= '<object type="application/x-shockwave-flash" data="//d.ratepay.com/WDWL/c.swf" width="0" height="0"><param name="movie" value="//d.ratepay.com/WDWL/c.swf" /><param name="flashvars" value="t='.$consumerDeviceId.'&v=WDWL"/><param name="AllowScriptAccess" value="always"/></object>';
-        }
+       $ratepay = "";
 
 	    return $ratepay;
     }
