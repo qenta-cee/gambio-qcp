a7f36f96dd9736c95101cda87637fa5399555990
Marco Horn <marco.horn@wirecard.com>
2018-12-21 13:04:08 +0100

    #43 prepare release

--- includes/modules/payment/wcp.php
+++ includes/modules/payment/wcp.php
@@ -40,7 +40,7 @@
 
 define('TABLE_PAYMENT_WCP', 'payment_wirecard_checkout_page');
 define('INIT_SERVER_URL', 'https://checkout.wirecard.com/page/init-server.php');
-define('WCP_PLUGIN_VERSION', '2.2.8');
+define('WCP_PLUGIN_VERSION', '2.2.9');
 define('WCP_PLUGIN_NAME', 'GambioGX2_WCP');
 define('MODULE_PAYMENT_WCP_WINDOW_NAME', 'wirecardCheckoutPageIFrame');
 define('COMPARE_SHOP_VERSION', 'v3.9');
